a930d4eb67c9ccc68b1820470c5d28000025d645
Salvatore Basile <saxxas88@gmail.com>
2022-12-16 18:21:17 +0100

    Update component

--- src/components/Hero/Hero_w_OveralyImage.js
+++ src/components/Hero/Hero_w_OveralyImage.js
@@ -6,7 +6,7 @@ function Hero_w_OveralyImage({opacity_value, url_bg, title, info, cta}) {
   <div className={`hero-overlay bg-opacity-${opacity_value}`}></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">{title}</h1>
+      <h1 className="mb-5 text-5xl font-bold baloo_da">{title}</h1>
       <p className="mb-5">{info}</p>
       <button className="btn btn-primary" onClick={cta?.onClickCTA}>{cta?.title}</button>
     </div>
